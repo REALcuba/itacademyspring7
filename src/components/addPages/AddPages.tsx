e7ac29d01ce9d71618e0a2727834f61d83016e42
REALcuba <angelskcuba@gmail.com>
2023-07-17 10:11:12 +0200

    nivel 2 ejercicio 6,hecho pero no acepta valores el input  2

--- src/components/addPages/AddPages.tsx
+++ src/components/addPages/AddPages.tsx
@@ -1,40 +1,52 @@
 import React, { ChangeEvent } from 'react'
+import { InfoPagesBtn, InfoLanguagesBtn } from '../infoBtn/InfoBtn'
 
 type AddPagesProps = {
     isChecked: boolean,
-    handleInputPagesChange: (e: ChangeEvent<HTMLInputElement>, position: number) => void,
-    handleInputLanguagesChange: (e: ChangeEvent<HTMLInputElement>, position: number) => void,
     valuePages: number,
     valueLanguages: number,
     index: number,
+    handleInputPagesChange: (e: ChangeEvent<HTMLInputElement>, position: number) => void,
+    handleInputLanguagesChange: (e: ChangeEvent<HTMLInputElement>, position: number) => void,
     handleAddPageBtn: (position: number) => void,
     handleSubstractPageBtn: (position: number) => void,
     handleAddLanguageBtn: (position: number) => void,
     handleSubstractLanguageBtn: (position: number) => void,
-    // pagesArr: number[],
-    // languagesArr: number[]
-}
-const AddPages: React.FC<AddPagesProps> = ({ isChecked, valuePages, valueLanguages, handleInputPagesChange, index, handleInputLanguagesChange, handleAddPageBtn, handleSubstractPageBtn, handleAddLanguageBtn, handleSubstractLanguageBtn }) => {
-    const className = isChecked !== true ? "d-none" : "border border-2 border-black p-4 rounded-2 "
 
+}
+const AddPages: React.FC<AddPagesProps> = ({
+    isChecked,
+    valuePages,
+    valueLanguages,
+    index,
+    handleInputPagesChange,
+    handleInputLanguagesChange,
+    handleAddPageBtn,
+    handleSubstractPageBtn,
+    handleAddLanguageBtn,
+    handleSubstractLanguageBtn }) => {
+    const className = isChecked !== true ? "d-none" : "border border-2 p-4 rounded-2 "
+
+    console.log(`valuepage:${valuePages}`);
 
     return (
 
         <div className={className}>
 
-            <div>
-                <span>Número de páginas</span>
-                <button type="button" className='btn btn-primary' onClick={() => handleAddPageBtn(index)}>+</button>
-                <input type="text" name="" id="" value={valuePages} onChange={(e) => handleInputPagesChange(e, index)} />
-                <button type="button" className='btn btn-primary' onClick={() => handleSubstractPageBtn(index)}>-</button>
+            <div className='d-flex justify-content-between'>
+                <span>Número de páginas:</span>
+                <button type="button" className='btn btn-primary btn-sm' onClick={() => handleAddPageBtn(index)}>+</button>
+                <input type="text" className='w-25' value={valuePages} onChange={(e) => handleInputPagesChange(e, index)} />
+                <button type="button" className='btn btn-primary btn-sm' onClick={() => handleSubstractPageBtn(index)}>-</button>
+                <InfoPagesBtn pages={valuePages} />
             </div>
 
-            <div>
-                <span>Número de idiomas</span>
-                <button type="button" className='btn btn-primary' onClick={() => handleAddLanguageBtn(index)}>+</button>
-                <input type="text" value={valueLanguages} name="" id="" onChange={(e) => handleInputLanguagesChange(e, index)} />
-                <button type="button" className='btn btn-primary' onClick={() => handleSubstractLanguageBtn(index)}>-</button>
-
+            <div className='d-flex justify-content-between'>
+                <span>Número de idiomas:</span>
+                <button type="button" className='btn btn-primary btn-sm' onClick={() => handleAddLanguageBtn(index)}>+</button>
+                <input type="text" value={valueLanguages} className='w-25' onChange={(e) => handleInputLanguagesChange(e, index)} />
+                <button type="button" className='btn btn-primary btn-sm' onClick={() => handleSubstractLanguageBtn(index)}>-</button>
+                <InfoLanguagesBtn languages={valueLanguages} />
             </div>
         </div >
 
--- src/pages/MainBoard/MainBoard.tsx
+++ src/pages/MainBoard/MainBoard.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import Input from "../../components/input/Input";
 import AddPages from "../../components/addPages/AddPages";
+import { useLocalStorage } from '../../components/customHooks/UseLocalStorage'
 
 type MainBoardProps = {
     services: {
@@ -9,15 +10,25 @@ type MainBoardProps = {
     }[];
 
 }
+// type useLocalStorageType<T> = [
+//     T,
+//     React.Dispatch<React.SetStateAction<T>>,
+// ];
+// Get localstorage pagesArr
+// const pagesArrFromLocalstorage: number[] | null = JSON.parse(window.localStorage.getItem('isLocalePageArr'));
+// console.log("pagesArrFromLocalstorage", pagesArrFromLocalstorage)
 
+// Usar datos de localstorage para inicializar estados
 export const MainBoard: React.FC<MainBoardProps> = ({ services }) => {
     const [total, setTotal] = useState(0)
     const [isCheckArr, setIsCheckArr] = useState(new Array(services.length).fill(false))
     const [isChecked, setIsChecked] = useState(false);
     const [pagesArr, setPagesArr] = useState(new Array(services.length).fill(1))
     const [languagesArr, setLanguagesArr] = useState(new Array(services.length).fill(1))
-    const [pages, setPages] = useState(1)
-    const [languages, setLanguages] = useState(1)
+    const [pages, setPages] = useState<number>(1)
+    const [languages, setLanguages] = useState<number>(1)
+    const [isLocalePageArr, setIsLocalePageArr] = useLocalStorage('isLocalePageArr', new Array(services.length).fill(1))
+    const [isLocaleLanguagesArr, setIsLocaleLanguagesArr] = useLocalStorage('isLocaleLanguagesArr', new Array(services.length).fill(1))
 
     const updatedPagesArr = [...pagesArr]
     const updatedLanguagesArr = [...languagesArr];
@@ -35,48 +46,69 @@ export const MainBoard: React.FC<MainBoardProps> = ({ services }) => {
             setTotal(totalPrice);
         }
     }
+    // const handleAddPageBtn = (position: number): void => {
+    //     console.log('suma');
+
+    //     // setPages(pages + 1);
+    //     console.log(pages);
+    //     updatedPagesArr[position] = pages+1;
+    //     console.log(updatedPagesArr[position]);
+
+    //     setPagesArr(updatedPagesArr);
+    //     setIsLocalePageArr(updatedPagesArr);
+
+    // };
     const handleAddPageBtn = (position: number): void => {
         console.log('suma');
 
-        setPages(pages + 1);
         console.log(pages);
-        updatedPagesArr[position] = pages + 1;
-        setPagesArr(updatedPagesArr);
+        updatedPagesArr[position] = updatedPagesArr[position] + 1;
+        console.log(updatedPagesArr[position]);
 
+        setPagesArr(updatedPagesArr);
+        setIsLocalePageArr(updatedPagesArr);
     };
+
+
     const handleAddLanguageBtn = (position: number): void => {
         console.log('suma lenguajes');
         // if (updatedLanguagesArr[position] >= 1) {
-        setLanguages(languages + 1);
+        // setLanguages(languages + 1);
         console.log(languages);
-        updatedLanguagesArr[position] = languages + 1;
+        updatedLanguagesArr[position] = updatedLanguagesArr[position] + 1;
         setLanguagesArr(updatedLanguagesArr);
+        setIsLocaleLanguagesArr(updatedLanguagesArr);
         // }
     };
 
     const handleSubstractPageBtn = (position: number): void => {
         console.log('resta');
-        if (pages > 1) {
+        if (updatedPagesArr[position] > 1) {
             console.log('suma');
-            setPages(pages - 1);
+            // setPages(pages - 1);
             console.log(pages);
-            updatedPagesArr[position] = pages - 1;
+            updatedPagesArr[position] = updatedPagesArr[position] - 1;
             setPagesArr(updatedPagesArr);
+            setIsLocalePageArr(updatedPagesArr);
+
         }
     }
+    console.log(pagesArr);
+
     const handleSubstractLanguageBtn = (position: number): void => {
         console.log('resta');
-        if (languages > 1) {
+        if (updatedLanguagesArr[position] > 1) {
             console.log('suma');
-            setLanguages(languages - 1);
+            // setLanguages(languages - 1);
             console.log(languages);
-            updatedLanguagesArr[position] = languages - 1;
+            updatedLanguagesArr[position] = updatedLanguagesArr[position] - 1;
             setLanguagesArr(updatedLanguagesArr);
+            setIsLocaleLanguagesArr(updatedLanguagesArr);
         }
     }
     const handleInputPagesChange = (e: ChangeEvent<HTMLInputElement>, position: number): void => {
         updatedPagesArr[position] = parseInt(e.target.value);
-        setPagesArr(updatedPagesArr);
+        setPagesArr(updatedPagesArr[position]);
 
     };
     const handleInputLanguagesChange = (e: ChangeEvent<HTMLInputElement>, position: number): void => {
@@ -85,7 +117,7 @@ export const MainBoard: React.FC<MainBoardProps> = ({ services }) => {
     }
     
     const handleOnChange = (position: number): void => {
-
+        // e.stopPropagation();
 
         const updatedCheckedState: boolean[] = isCheckArr.map((item, index) =>
             index === position ? !item : item
@@ -94,7 +126,7 @@ export const MainBoard: React.FC<MainBoardProps> = ({ services }) => {
 
         updatedCheckedState.forEach((currentState: boolean, index) => {
             if (currentState === true) {
-                setPages(1)
+                // setPages(1)
                 setIsChecked(currentState);
                 setPages(pagesArr[index]);
                 setLanguages(languagesArr[index]);
@@ -103,23 +135,46 @@ export const MainBoard: React.FC<MainBoardProps> = ({ services }) => {
             }
         });
     };
+    // const handleInputBlur = (index: number) => {
+    //     // Realizar alguna lógica adicional cuando se pierde el foco del input en el índice específico
+    //     console.log(`Input ${index} perdió el foco`);
+
+    //     // Ejemplo: Validar el valor del input
+    //     const inputValue = pagesArr[index];
+    //     if (inputValue < 0) {
+    //         console.log(`El valor del input ${index} es inválido`);
+    //         // Realizar alguna acción en caso de valor inválido
+    //     }
+    // };
+    // const handleInputBlur = (index: number): void => {
+    //     // console.log(`Input ${index} perdió el foco`);
+    //     // Realizar lógica adicional cuando se pierde el foco del input en el índice específico
+    //     // const inputValue = pagesArr[index];
+    //     if (isCheckArr[index] === true) {
+    //         console.log(`El valor del input ${index} es ${isChecked}`);
+    //         // Realizar alguna acción en caso de valor inválido
+    //         stopPropagation()
+    //     }
+    // };
+
     useEffect(() => {
         calculateTotal(pagesArr, languagesArr, isCheckArr, services);
-
-    }, [pagesArr, languagesArr, isCheckArr, services])
+        setPagesArr(isLocalePageArr);
+        setLanguagesArr(isLocaleLanguagesArr);
+    }, [pagesArr, languagesArr, isCheckArr, services, isLocalePageArr, isLocaleLanguagesArr])
 
     return (
-        <>
+        <section>
             <form >
                 <label >
                     <strong>Que quieres hacer?</strong>
                     {services.map(({ project, price }, index) => (
                         <div key={index}>
                             <div>
-                                {<Input handleOnChange={handleOnChange}
+                                {<Input
+                                    handleOnChange={(index) => handleOnChange(index)}
                                     index={index}
-                                    isChecked={isCheckArr[index]}
-                                    checkedArr={isCheckArr}
+                                    // handleInputBlur={() => handleInputBlur(index)}
                                 />}
                                 {project} ({price} €)
                             </div>
@@ -139,6 +194,6 @@ export const MainBoard: React.FC<MainBoardProps> = ({ services }) => {
                 </label>
             </form>
             <div>Price: {total}€</div>
-        </>
+        </section>
     )
 }
